bd0944948717dd24530557ef5956891f3f2401da
Lambson <slambson@SDGL09d5c9fac.local>
2012-01-06 16:08:15 -0700

    Added the actiongroup javascript files for manage tests and run tests.  Registered coming soon pages for all the pages we have icons for in the images directory. Made a change in index.html to change managetests to testmgt since that is what all the icons are named for test management.

---
+++ slickij-war/src/main/webapp/js/pages/managetests.js
@@ -0,0 +1,10 @@
+/* 
+ * Registration of Manage Tests Pages.
+ */
+
+Pages.addGroup("testmgmt", "Manage Tests", true);
+Pages.group("testmgmt").registerPage("viewproject", "View Project", true).comingSoon();
+Pages.group("testmgmt").registerPage("addbuild", "Add Build", true).comingSoon();
+Pages.group("testmgmt").registerPage("addrelease", "Add Release", true).comingSoon();
+Pages.group("testmgmt").registerPage("addtest", "Add Test Case", true).comingSoon();
+Pages.group("testmgmt").registerPage("findtests", "Find Test Cases", true).comingSoon();
---
+++ slickij-war/src/main/webapp/js/pages/runtests.js
@@ -0,0 +1,9 @@
+/* 
+ * Registration of Run Tests Pages.
+ */
+
+Pages.addGroup("runtests", "Run Tests", true);
+Pages.group("runtests").registerPage("addtp", "Add Test Plan", true).comingSoon();
+Pages.group("runtests").registerPage("findtp", "Find Test Plan", true).comingSoon();
+Pages.group("runtests").registerPage("manualresults", "Enter Manual Results", true).comingSoon();
+Pages.group("runtests").registerPage("machinestatus", "Machine Status", true).comingSoon();
