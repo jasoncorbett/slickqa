a752ebf092f778faa7f15fcb4b101e0bf7a5d1ee
Rahul Shyamsukha <rahul_shyamsukha@intuit.com>
2012-01-31 23:55:31 +0530

    Changes related to View Project page

--- slickij-war/src/main/webapp/js/pages/managetests.js
+++ slickij-war/src/main/webapp/js/pages/managetests.js
@@ -3,7 +3,7 @@
  */
 
 Pages.addGroup("testmgmt", "Manage Tests", true);
-Pages.group("testmgmt").registerPage("viewproject", "View Project", true).comingSoon();
+Pages.group("testmgmt").registerPage("viewproject", "View Project", true);
 Pages.group("testmgmt").registerPage("addbuild", "Add Build", true).comingSoon();
 Pages.group("testmgmt").registerPage("addrelease", "Add Release", true).comingSoon();
 Pages.group("testmgmt").registerPage("addtest", "Add Test Case", true).comingSoon();
