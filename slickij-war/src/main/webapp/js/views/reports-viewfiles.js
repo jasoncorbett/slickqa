89d7ee98fbd15b750542ad2f30736f6a850d04f6
Jaron <jaron@jaron.local>
2012-03-20 09:25:48 -0600

    Version 2 of hte view files page.  Added logic into the template to generate the file list dynamically.

--- slickij-war/src/main/webapp/js/views/reports-viewfiles.js
+++ slickij-war/src/main/webapp/js/views/reports-viewfiles.js
@@ -2,18 +2,21 @@ var ReportsViewFilesPage = SlickPage.extend({
     name: "Files For Failed Test Cases",
     codename: "viewfiles",
     group: "reports",
-    navigation: false,
-    
+    //TODO: Set this to false
+    navigation: true,
+
     initialize: function() {
         if(! this.options.result) {
             this.requiredData = {
                 "results": function() {
-                    return "api/results/" + this.options.positional[0];
+                    //return "api/results/" + this.options.positional[0];
+                    //TODO: Remove this line
+                    return "api/results/4f5fc2f53677706940bb0702";
                 }
             };
         }
         this.on("ready", this.onReady, this);
-        //this.on("finish", this.onFinish, this);
+        this.on("finish", this.onFinish, this);
     },
 
     onReady: function() {
@@ -25,28 +28,61 @@ var ReportsViewFilesPage = SlickPage.extend({
         this.title = "File List For Testcase: '" + this.result.testcase.name + "'";
     },
 
-/*    onFinish: function() {
-        var tabledata = [];
-        _.each(this.data.results.files, function(files) {
-            tabledata[tabledata.length] = [safeReference(files, "filename", ""),
-                                           safeReference(files, "mimetype", ""),
-                                           safeReference(files, "uploadDate", "")];
-        });
+    onFinish: function() {
+//        $(".file").on("click", { page: this }, this.displayFiles());
+        console.log("build the dang filelist!!");
+        this.buildFileList();
 
-        $("#file-detail-table").dataTable({
-            aaData: tabledata,
-            aoColumns: [
-                {"sTitle": "Name", "sWidth": "60%"},
-                {"sTitle": "Mimetype", "sWidth": "40%"},
-                {"sTitle": "Date", "sWidth": "40%"}],
-            bJQueryUI: true,
-            bSort: true,
-            bAutoWidth: false,
-            bDeferRender: true,
-            bPaginate: false,
-            sDom: "<\"H\"lfr>tS<\"F\"ip>",
-            sScrollY: "" + ($(document).height() - (4 * $("#pagetitle").height()) -  (3 * $("#titlebar").height()) ) + "px"
+        prettyPrint();
+    },
+
+    buildFileList: function() {
+        _.each(this.result.files, function(file) {
+
+            var mimeType = file.mimetype;
+            var fileName = file.filename;
+            var fileId   = file.id;
+            var html = "";
+
+            if (mimeType.indexOf("text/html") != -1) {
+
+                html += "<div class=\"fileicon\">";
+                html += "<img class=\"file box\" src=\"/images/htmlicon.png\" alt=\"" + fileName + "\"/>";
+                html += "<a class=\"label filename\" href=\"/api/files/" + fileId + "/content/" + fileName + "\">" + fileName + "</a>";
+                html += "</div>";
+
+            } else if (mimeType.indexOf("image/png") != -1) {
+
+                html += "<div class=\"fileicon\">";
+                html += "<img class=\"file box\" src=\"/images/htmlicon.png\" alt=\"" + fileName + "\"/>";
+                html += "<a class=\"label filename\" href=\"/api/files/" + fileId + "/content/" + fileName + "\">" + fileName + "</a>";
+                html += "</div>";
+                //console.log("<pre class=\"prettyprint lang-html\">" /api/files/" + fileId + "/content/" + fileName + "\">" + fileName + "</a>");
+            }
+            console.log("jaron is the man");
+            console.log(html);
+            console.log("jaron is the man 2X");
+            $('#filelist').append(html);
         });
-    } */
+    },
+
+    displayFile: function(event) {
+        console.log("testing");
+//        _.each(this.result.files, fucntion(id, filename, mimetype)
+//        {
+//
+//        }
+//        )
+//
+//        );
+
+        //build the contents of hte file display div
+        //if mime type = text/html
+        // render <pre> tags
+        //else mime type = image/png
+        //files.id
+        //page.template(name, filename, $('filedisplaycontainter'));
+        //http://localhost:8080/api/files/{{id}}/content/{{filename}}" alt="{{filename}}
+    }
 
 });
