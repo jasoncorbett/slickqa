d7de3cf8bf5c8792663a0e59ad651990f2b6b30d
Jason Corbett <Jason_Corbett@intuit.com>
2012-04-30 14:08:43 -0600

    removing "pages" directory as it is not used anymore

--- slickij-war/src/main/webapp/js/pages/dashboard.js
+++
@@ -1,6 +0,0 @@
-/* 
- * Registration of Dashboard Pages.
- */
-
-Pages.addGroup("dashboards", "Dashboards", true);
-Pages.group("dashboards").registerPage("main", "Main Dashboard", true);
--- slickij-war/src/main/webapp/js/pages/reports.js
+++
@@ -1,4 +0,0 @@
-Pages.addGroup("reports", "Reports", true);
-Pages.group("reports").registerPage("bytestrun", "Test Run Results", true);
-Pages.group("reports").registerPage("comparetestrun", "Compare Test Run", true).comingSoon();
-
