db516a05cda7295da7cd73551d7d9432f7c42d71
Jason Corbett <Jason_Corbett@intuit.com>
2012-08-19 19:38:51 -0600

    first step towards testrun group report page

--- slickij-war/src/main/webapp/js/views/reports-testrungroup.js
+++ slickij-war/src/main/webapp/js/views/reports-testrungroup.js
@@ -12,8 +12,8 @@ var ReportsResultViewPage = SlickPage.extend({
     navigation: true,
 
     requiredData: {
-        result: function() {
-            return "api/testrungroups";
+        testrungroups: function() {
+            return "api/testrungroups?createdafter=" + moment().subtract('months', 3).valueOf();
         }
     },
 
@@ -27,8 +27,8 @@ var ReportsResultViewPage = SlickPage.extend({
         var tbldata = [];
         _.each(this.data.testrungroups, function(testrungroup) {
             var resultBar = "";
-            _.each(testrungroup.summary.statusListOrdered, function(statusName) {
-                resultBar = resultBar + "<div class=\"result-bar-inner status-background-" + statusName.replace("_", "") + "\" style=\"width: " + (testrungroup.summary.resultsByStatus[statusName] / testrungroup.summary.total).toFixed(2) + "in;\"></div>";
+            _.each(testrungroup.groupSummary.statusListOrdered, function(statusName) {
+                resultBar = resultBar + "<div class=\"result-bar-inner status-background-" + statusName.replace("_", "") + "\" style=\"width: " + (testrungroup.groupSummary.resultsByStatus[statusName] / testrungroup.groupSummary.total).toFixed(2) + "in;\"></div>";
             });
 
 
@@ -37,14 +37,32 @@ var ReportsResultViewPage = SlickPage.extend({
                 "<div class=\"result-bar center-block\">" + resultBar + "</div>",
                 safeReference(testrungroup, "groupSummary.total", 0),
                 safeReference(testrungroup, "testruns.length", 0),
-                "<a href=\"javascript:;\" class=\"button testrungroup-edit-button\" data-id=\"" + testrungroup.id + "\" > Edit </a>",
+                "<a href=\"javascript:;\" class=\"button testrungroup-edit-button\" data-id=\"" + testrungroup.id + "\" > Edit </a>" +
                 "<a href=\"javascript:;\" class=\"button testrungroup-delete-button\" data-id=\"" + testrungroup.id + "\"> Delete </a>",
-                new Date(testrungroup.dateCreated)];
+                new Date(testrungroup.created)];
         });
-        this.testrunData = tbldata;
+        this.testrungroupData = tbldata;
     },
 
     onFinish: function() {
+        var datatable = $("#reports-testrungroup-table").dataTable({
+            aaData: this.testrungroupData,
+            aoColumns: [
+                {"sTitle": "Name", "sWidth": "40%", "sType": "html"},
+                {"sTitle": "Results", "sWidth": "10%", "sType": "html", "sClass": "center"},
+                {"sTitle": "Total", "sWidth": "5%", "sClass": "center"},
+                {"sTitle": "Total Testruns", "sWidth": "5%", "sClass": "center"},
+                {"sTitle": "Actions", "sWidth": "20%", "sClass": "center"},
+                {"sTitle": "Date Created", "sWidth": "20%", "sClass": "center"}],
+            bJQueryUI: true,
+            bAutoWidth: false,
+            bDeferRender: true,
+            bPaginate: false,
+            sDom: "<\"H\"lfr>tS<\"F\"ip>",
+            sScrollY: "" + ($("#content").height() - (4 * $("#bytestrun-release-select").height()) - (5 * $("#footer").height()) - $("#content-bottom-pad").height()) + "px"
+        });
+        datatable.fnSort([[5, "desc"]]);
+
 
     }
 });
